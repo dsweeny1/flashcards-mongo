7084d0b956b8cc8d4bb78cb8d6bbcbba2fe2499d
Danielle Sweeny <dsweeny1@gmail.com>
2023-02-06 08:51:43 -0700

    Fix CORS

--- server.js
+++ server.js
@@ -17,9 +17,12 @@ app.locals.title = 'Flashcards Mongo';
 const studentsRouter = require('./routes/students')
 
 app.use('/api/v1/students', studentsRouter)
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next()
 })
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => console.log(`Connection Made`))
