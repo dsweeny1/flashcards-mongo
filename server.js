a52e49cec113715493e077c49f54233f11ebd3bb
Danielle Sweeny <dsweeny1@gmail.com>
2023-02-06 08:55:37 -0700

    fix CORS

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use('/api/v1/students', studentsRouter)
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*")
 
     next()
 })
