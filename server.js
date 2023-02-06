c38b333b7dc4564e00727632b3af1e9160109866
Danielle Sweeny <dsweeny1@gmail.com>
2023-02-06 08:49:02 -0700

    Fix CORS issue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const cors = require('cors');
 
 const express = require('express')
 const app = express()
@@ -8,6 +9,7 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(`${error.message}`))
 db.once('open', () => console.log('Connected to Database'))
 
+app.use(cors());
 app.use(express.json())
 
 app.locals.title = 'Flashcards Mongo';
@@ -15,6 +17,10 @@ app.locals.title = 'Flashcards Mongo';
 const studentsRouter = require('./routes/students')
 
 app.use('/api/v1/students', studentsRouter)
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => console.log(`Connection Made`))
 
